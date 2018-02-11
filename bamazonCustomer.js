78798e5820f3b70423ee544465c552a3d1eebff2
shivraxy <31151144+shivraxy@users.noreply.github.com>
2018-02-10 22:26:28 -0800

    New Changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,6 +8,7 @@ connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
+    password: "",
     database: "bamazon"
 });
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -7,6 +7,7 @@ connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
+    password: "",
     database: "bamazon"
 });
 
